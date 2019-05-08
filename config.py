f6e021b515378d19bccfe46cee4427b22bebf7d7
Rolf <rolf.wessels@gmail.com>
2019-05-08 19:20:20 +0200

    Adding config and main program is ready.

--- config.py
+++ config.py
@@ -1,13 +1,65 @@
-from enum import IntEnum
 import RPi.GPIO as GPIO
 
 
-class Pins(IntEnum):
-    TANK_1_DUMP = 19
-    BUTTON_1 = 20
-    BUTTON_2 = 21
-    BUTTON_3 = 26
-    START = 16
+class Pins:
+    FullRun = 19
+    Tank1Empty = 20
+    Tank2Empty = 21
+    KegEmpty = 26
+    StartStop = 16
+
+    _fullRun = False
+    _tank1Empty = False
+    _tank2Empty = False
+    _kegEmpty = False
+    _startStop = False
+
+    @staticmethod
+    def all_pins():
+        return {"Fullrun": Pins._fullRun,
+                "Tank 1 Empty": Pins._tank1Empty,
+                "Tank 2 Empty": Pins._tank2Empty,
+                "Keg Empty": Pins._kegEmpty,
+                "Start": Pins._startStop}
+
+    @staticmethod
+    def read_all():
+        Pins._fullRun = GPIO.input(Pins.FullRun) == 1
+        Pins._tank1Empty = GPIO.input(Pins.Tank1Empty) == 1
+        Pins._tank2Empty = GPIO.input(Pins.Tank2Empty) == 1
+        Pins._kegEmpty = GPIO.input(Pins.KegEmpty) == 1
+        Pins._startStop = GPIO.input(Pins.StartStop) == 1
+
+    @staticmethod
+    def is_fullRun_enabled():
+        return Pins._fullRun
+    @staticmethod
+    def is_tank1Empty_enabled():
+        return Pins._tank1Empty
+    @staticmethod
+    def is_tank2Empty_enabled():
+        return Pins._tank2Empty
+    @staticmethod
+    def is_kegEmpty_enabled():
+        return Pins._kegEmpty
+    @staticmethod
+    def is_started():
+        return Pins._startStop
+
+
+    @staticmethod
+    def get_fullRun():
+        return Pins._fullRun
+
+    @staticmethod
+    def start_stop_click(channel):
+        Pins._startStop = not Pins._startStop
+        print("Start", "[on]" if (Pins._startStop == 1)else "[off]")
+
+    @staticmethod
+    def print():
+        for key, value in Pins.all_pins().items():
+            print(key, "[on]" if value else "[off]")
 
 
 # Setup input and output
@@ -15,5 +67,13 @@ print("Configure GPIO")
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup([Pins.TANK_1_DUMP, Pins.BUTTON_1, Pins.BUTTON_2,
-            Pins.BUTTON_3, Pins.START], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup([Pins.FullRun, Pins.Tank1Empty, Pins.Tank2Empty,
+            Pins.KegEmpty, Pins.StartStop], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(Pins.StartStop, GPIO.FALLING,
+                      callback=Pins.start_stop_click, bouncetime=300)
+Pins.read_all()
+
+
+# GPIO.add_event_detect([Pins.FullRun, Pins.Tank1Empty, Pins.Tank2Empty,
+#                        Pins.KegEmpty, Pins.StartStop], GPIO.FALLING, callback=Pins.read_all, bouncetime=300)
--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-print('PMP 1 OFF')
-from config import Color
-import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+from config import Pins
+import time
+import sys
 
 
+# class MyClass:
+#     "This is my second class"
+#     a = 10
 
-class MyClass:
-	"This is my second class"
-	a = 10
-	def func(self):
-		print('Hello')
-		return "asdf"
+#     def func(self):
+#         print('Hello')
+#         return "asdf"
 
-print(repr(Color.RED))
 
-def my_callback(channel):  
-        print ("tank "+str(channel)+" empty")
-print(MyClass().func())
-my_callback(1)
+# def my_callback(channel):
+#     print("tank "+str(channel)+" empty")
+
+
+# print(MyClass().func())
+# my_callback(1)
+
+try:
+    while True:
+        print('------------------')
+        print('Program ready.')
+        Pins.print()
+        print('Press button to start.')
+        print('------------------')
+        print('')
+
+        while not Pins.is_started():
+            time.sleep(0.5)
+            pass
+
+        print('Starting')
+        print('press [start_stop] to stop.')
+        print("  Done   ")
+        Pins._startStop = False
+
+
+except KeyboardInterrupt:
+    print('Stopping')
+    sys.exit
